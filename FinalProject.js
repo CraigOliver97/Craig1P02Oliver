349c0f862864a7eb07d3ede019b2c1eee8f5c165
CraigOliver97 <31972292+CraigOliver97@users.noreply.github.com>
2017-12-06 12:41:01 -0500

    Add files via upload

---
+++ FinalProject.js
@@ -0,0 +1,56 @@
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+function myFunction() {
+
+    document.getElementById("myText").value = "Whoville";
+}
+
+function myFunction1() {
+
+    document.getElementById("myText1").value = "Saint Nicholas";
+}
+
+function myFunction2() {
+
+    document.getElementById("myText2").value = "Four Calling Birds";
+}
+
+function myFunction3() {
+
+    document.getElementById("myText3").value = "The Polar Express";
+}
+
+function myFunction4() {
+
+    document.getElementById("myText4").value = "The Beatles";
+}
